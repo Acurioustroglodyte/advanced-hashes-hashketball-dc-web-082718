1a1f2b7d822e171aefc664f2ced898323ac693f0
Trevor Jameson <tjameson15@gmail.com>
2018-07-30 16:22:17 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,6 +176,7 @@ def big_shoe_rebounds()
     team_data[:players].each do |player, player_data|
       # Did not find method for easily converting array to fixnum
       # Used [0] to grab the first and only fixnum value instead
+      binding.pry
       if player_data[:shoe] > biggest_shoe.values[0]
         biggest_shoe = {player => player_data[:shoe][0]}
       end
