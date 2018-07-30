382fd062ed5d17650ed7d636edee27cae3b4090c
Trevor Jameson <tjameson15@gmail.com>
2018-07-30 16:31:32 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,6 +182,6 @@ def big_shoe_rebounds()
     end
     # Return the player with the biggest shoes' rebounds
     binding.pry
-    return team_data[:players][biggest_shoe]
+    return team_data[:players][biggest_shoe.keys[0]]
   end
 end
